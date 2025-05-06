84895276a78d574609dd7b4d1ec7df6cc9db96a4
B20DCCN520_Hoàng Duy Phương <hoangduyphuong30102002@gmail.com>
2025-05-06 19:39:08 +0700

    Sqrt

---
+++ leetcode/Sqrt.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+public class Sqrt {
+    public static void main(String[] args) {
+        System.out.println(sqrt(3));
+        System.out.println(sqrt(4));
+        System.out.println(sqrt(8));
+        System.out.println(sqrt(-5));
+    }
+
+    static int sqrt(int x) {
+        return (int)Math.sqrt(x);
+    }
+}
